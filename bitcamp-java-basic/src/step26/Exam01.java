3e6150f39b07f8ed8502182f258443f8cb655ded
soyoungkim9 <soyoung930623@gmail.com>
2018-05-03 11:52:35 +0900

    .

--- bitcamp-java-basic/src/step26/Exam01.java
+++ bitcamp-java-basic/src/step26/Exam01.java
@@ -11,7 +11,6 @@ public class Exam01 {
      
     static void init() throws Exception {
         String value = System.getProperty("country");
-        System.out.println(value);
         if (value != null) {
             country = value;
         }
@@ -23,7 +22,12 @@ public class Exam01 {
     
     public static void main(String[] args) throws Exception {
         init();
-        System.out.println("안녕하세요!");
+        // 다음과 같이 화면에 텍스트를 출력할 때 직접 입력한 값으 출력하는 것이 아니라
+        // 프로퍼티 파일에서 읽은 값을 출력한다.
+        // 이렇게 한 이유는 다양한 언어로 출력하기 위함이다.
+        // 즉 다국어를 제공할 수 있도록 개발된 프로그램을
+        // "국제화(i18n;Internationalization)"라고 얘기한다.
+        System.out.println(label.get("welcome"));
     }
 
 }
