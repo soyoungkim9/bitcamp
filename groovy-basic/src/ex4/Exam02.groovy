9c614b1e76670ba9ec6f4e5a203922fd93aa7df9
soyoungkim9 <soyoung930623@gmail.com>
2018-05-04 12:28:23 +0900

    .

--- groovy-basic/src/ex4/Exam02.groovy
+++
@@ -1,8 +0,0 @@
-// 메서드 - 파라미터
-package ex4
-
-obj = new Exam01()
-println obj.hello()
- 
- 
- 
---
+++ spring-ioc/src/main/java/bitcamp/java106/step02/Exam01.java
@@ -0,0 +1,18 @@
+// 스프링 IoC 컨테이너
+package bitcamp.java106.step02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bitcamp.java106.BeanUtils;
+
+public class Exam01 {
+
+    public static void main(String[] args) {
+        ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+                "bitcamp/java106/step02/application-context-01.xml");
+        BeanUtils.printBeanList(iocContainer);
+        
+    }
+
+}
