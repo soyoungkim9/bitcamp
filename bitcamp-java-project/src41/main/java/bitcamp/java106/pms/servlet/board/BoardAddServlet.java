537b866a39ddbd3435caca44bed4c17087fdb17a
soyoungkim9 <soyoung930623@gmail.com>
2018-05-24 11:03:44 +0900

    2018-05-24-목

---
+++ bitcamp-java-project/src41/main/java/bitcamp/java106/pms/servlet/board/BoardAddServlet.java
@@ -0,0 +1,69 @@
+package bitcamp.java106.pms.servlet.board;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+
+import bitcamp.java106.pms.dao.BoardDao;
+import bitcamp.java106.pms.domain.Board;
+import bitcamp.java106.pms.support.WebApplicationContextUtils;
+
+@WebServlet("/board/add")
+public class BoardAddServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    
+    BoardDao boardDao;
+    
+    @Override
+    public void init() throws ServletException {
+        ApplicationContext iocContainer =
+                WebApplicationContextUtils.getWebApplicationContext(
+                        this.getServletContext());
+        boardDao = iocContainer.getBean(BoardDao.class);
+    }
+    
+    @Override
+    protected void doPost(
+            HttpServletRequest request, 
+            HttpServletResponse response) throws ServletException, IOException {
+        
+        try {
+            Board board = new Board();
+            board.setTitle(request.getParameter("title"));
+            board.setContent(request.getParameter("content"));
+
+            boardDao.insert(board);
+            response.sendRedirect("list");
+            
+        } catch (Exception e) {
+            RequestDispatcher 요청배달자 = request.getRequestDispatcher("/error");
+            request.setAttribute("error", e);
+            request.setAttribute("title", "게시물 등록 실패!");
+            요청배달자.forward(request, response);
+        }
+    }
+
+}
+
+//ver 40 - 필터 적용
+//ver 39 - forward 적용
+//ver 38 - redirect 적용
+//ver 37 - BoardAddController 클래스를 서블릿으로 변경
+//         출력 결과를 HTML로 변경
+//         자동 Refresh 태그 추가
+//ver 31 - JDBC API가 적용된 DAO 사용
+//ver 28 - 네트워크 버전으로 변경
+//ver 26 - BoardController에서 add() 메서드를 추출하여 클래스로 정의. 
+
+
+
+
+
+
