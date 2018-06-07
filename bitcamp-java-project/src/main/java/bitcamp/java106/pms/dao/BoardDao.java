9802ab4baa38b6e486dcc001eee5f62d3e5bc965
soyoungkim9 <soyoung930623@gmail.com>
2018-06-07 17:16:55 +0900

    2018-06-07-목

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/dao/BoardDao.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/dao/BoardDao.java
@@ -1,12 +1,13 @@
 package bitcamp.java106.pms.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import bitcamp.java106.pms.domain.Board;
 
 public interface BoardDao {
     int delete(int no) ;
-    List<Board> selectList();
+    List<Board> selectList(Map<String,Object> params);
     int insert(Board board);
     int update(Board board);
     Board selectOne(int no);
--- bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam07_2.java
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam07_2.java
@@ -51,9 +51,26 @@ public class Exam07_2 {
         //
         //
         // 예) 클라이언트에서 URL에 다음과 같은 형식으로 데이터를 보낼 때,
-        //      .../exam07_2/m2;name=aaa;age=20
+        //      .../exam07_2/m2;test;name=aaa;age=20;tel=111-222
+        //    => value= "test"
+        //    => name = "aaa"
+        //    => age = 20
+        //    => tel = "111-2222"
         return String.format("m2(): %s, %s, %d, %s", 
                 value, name, age, tel);
+    }
+    
+    @GetMapping(value="m3/{team}/{task}",
+            produces="text/plain;charset=UTF-8")
+    @ResponseBody
+    public String m3(
+            @MatrixVariable(name= "name", pathVar="team") String name1,
+            @MatrixVariable(name= "name", pathVar="task") String name2) {
+        
+        // 예) 클라이언트에서 URL에 다음과 같은 형식으로 데이터를 보낼 때,
+        //      .../exam07_2/m3;name=teamA;qty=3/name=task100;state=1
+        return String.format("m3(): 팀명=%s, 작업명=%s", 
+                name1, name2);
     }
     
     // ServletContext 얻기
