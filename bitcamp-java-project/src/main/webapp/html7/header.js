22e04f1102a633fc05f17dac0b728bca057b5986
soyoungkim9 <soyoung930623@gmail.com>
2018-07-09 17:02:58 +0900

    2018-07-09

--- bitcamp-java-project/src/main/webapp/html7/header.js
+++ bitcamp-java-project/src/main/webapp/html7/header.js
@@ -1,5 +1,5 @@
 // div#header 태그에 /html/header.html 내용을 삽입한다.
-$.get("/bitcamp-java-project/html6/header.html", (data) => {
+$.get("/bitcamp-java-project/html7/header.html", (data) => {
 	$("#header").html(data);
 	loadLoginUser();
 });
@@ -11,11 +11,11 @@ function loadLoginUser() {
 		$("#logoutBtn").click((e) => {
 			e.preventDefault(); // 클릭했을 때 원래 하던 일이 있는데 그것을 하지 말라!
 			$.get("/bitcamp-java-project/json/auth/logout", () => {
-				location.href = "/bitcamp-java-project/html6/auth/login.html";
+				location.href = "/bitcamp-java-project/html7/auth/login.html";
 			});
 		});
 	}).fail(() => {
-		location.href = "/bitcamp-java-project/html6/auth/login.html";
+		location.href = "/bitcamp-java-project/html7/auth/login.html";
 	});
 	
 }
