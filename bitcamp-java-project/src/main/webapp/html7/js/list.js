fd00bec23718797620a4d9d3f31c3839cbfeb00b
soyoungkim9 <soyoung930623@gmail.com>
2018-07-11 15:01:20 +0900

    .

---
+++ bitcamp-java-project/src/main/webapp/html7/js/list.js
@@ -0,0 +1,9 @@
+var trTemplateSrc = $("#tr-template").html();
+
+// 위에서 준비한 템플릿 데이터를 가지고 HTML을 생성할 템플릿 엔진 준비
+var templateFn = Handlebars.compile(trTemplateSrc);
+
+$.getJSON(serverRoot + "/json/board/list", (data) => {
+		//tableBody.innerHTML = templateFn(list:data);
+    	$(tableBody).html(templateFn({list:data}));
+});
